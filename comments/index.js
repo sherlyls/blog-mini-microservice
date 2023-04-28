c5f1f651fa3f3a3a6a1a81e1b2d4216bfb4d4975
sherlyls <sherlyls862@gmail.com>
2023-04-28 17:57:08 +0700

    update comment service

--- comments/index.js
+++ comments/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-
+    res.send(commentsByPostId[req.params.id] || [])
 })
 
 app.post('/posts/:id/comments', (req, res) => {
@@ -20,6 +20,8 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({ id: commentId, content})
 
     commentsByPostId[req.params.id] = comments
+
+    res.status(201).send(comments)
 
 })
 
