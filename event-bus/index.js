be1015dad895f01ff42e7058a5be37eae2ac3b5d
sherlyls <sherlyls862@gmail.com>
2023-05-08 15:35:12 +0700

    update event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser)
 
-app.post('/posts', (req, res) => {
+app.post('/events', (req, res) => {
     const { event } = req.body
 
     axios.post('http://localhost:4000/events', event)
@@ -15,4 +15,6 @@ app.post('/posts', (req, res) => {
     res.send({ status: 'OK'})
 })
 
-app.listen(4005)
+app.listen(4005, () => {
+    console.log('Listening on 4005')
+})
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require ('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express();
 app.use(bodyParser.json())
@@ -13,7 +14,7 @@ app.get('/posts', (req, res) => {
     res.send(posts)``
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const { title } = req.body
     
@@ -22,7 +23,12 @@ app.post('/posts', (req, res) => {
         id, title
     }
 
-    console.log({kekmana: posts[id]})
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    })
 
     res.status(201).send(posts[id]);
 })
