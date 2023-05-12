aae755dc03fd5351432af549f913bfba05cce474
sherlyls <sherlyls862@gmail.com>
2023-05-12 15:22:24 +0700

    receive event

--- query/index.js
+++ query/index.js
@@ -1,7 +1,26 @@
-const exoress = require("express");
+const express = require("express");
 const bodyParser = require("body-parser")
 const cors = require("cors")
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
+
+app.get('/posts', (req, res) => {
+
+})
+
+// receive events from event-bus
+app.post('/events', (req, res) => {
+    const { type, data } = req.body
+
+  if (type === 'PostCreated') {
+    const { id, title } = data
+
+    post[id] = { id, title, cooment: []}
+  }
+})
+
+app.listen(4002, () => {
+    console.log('Listening 4002')
+})
